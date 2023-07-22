31c5c5f73532bdf6c6fa1de8f0dc47a8ad2f35e9
William Casarin <jb55@jb55.com>
2023-07-22 11:03:37 -0700

    test: assert size again

--- test.c
+++ test.c
@@ -108,7 +108,7 @@ static void test_parse_contact_list()
 	size = ndb_note_from_json((const char*)json, written, &note, buf, alloc_size);
 	printf("ndb_note_from_json size %d\n", size);
 	assert(size > 0);
-	//assert(size == 60678);
+	assert(size == 59058);
 
 	printf("contacts content '%s'\n", ndb_note_content(note));
 	write_file("test_contacts_ndb_note", (unsigned char *)note, size);
