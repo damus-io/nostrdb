dbb9cabf34f1f5dd58a4023e16a31929c0fa378e
William Casarin <jb55@jb55.com>
2023-12-27 14:55:17 -0800

    content_parser: add initial db decoders
    We need to pull the data out as well! Let's add some initial decoders.
    We still need tests to make sure it's working.

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
 with pkgs;
 mkShell {
-  buildInputs = [ autoreconfHook flatbuffers flatcc pkg-config ];
+  buildInputs = [ autoreconfHook flatbuffers flatcc gdb pkg-config ];
 
   LIBCLANG_PATH="${llvmPackages.libclang}/lib";
 }
