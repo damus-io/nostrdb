9f4cc23022fee889a1c07e5c17b548376dcf5c82
William Casarin <jb55@jb55.com>
2024-01-08 12:09:20 -0800

    perf: add some flamegraph helpers to makefile

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> {} }:
 with pkgs;
 mkShell {
-  buildInputs = [ autoreconfHook flatbuffers flatcc pkg-config ];
+  buildInputs = [ autoreconfHook flatbuffers flatcc pkg-config flamegraph ];
 
   LIBCLANG_PATH="${llvmPackages.libclang}/lib";
 }
