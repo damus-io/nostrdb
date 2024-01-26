damus-io/nostrdb

commit ba86ffd06912fe7aef3eb165de5e2f0d2a59665d

tce: add AUTH to-client-event
This was committed to damus, but this should be in nostrdb or else we
will lose it when we update.

Damus: 84cfeb16048c ("nip42: add initial relay auth support")
Link: https://groups.google.com/a/damus.io/g/patches/c/Zx3dk01e0yg/m/t59TsVkXAQAJ
Signed-off-by: Charlie Fish <contact@charlie.fish>
Signed-off-by: William Casarin <jb55@jb55.com>

src/nostrdb.c
@@ -4953,6 +4953,17 @@ int ndb_ws_event_from_json(const char *json, int len, struct ndb_tce *tce,
 		tce->command_result.msglen = toksize(tok);
 
 		return 1;
+	} else if (tok_len == 4 && !memcmp("AUTH", json + tok->start, 4)) {
+		tce->evtype = NDB_TCE_AUTH;
+
+		tok = &parser.toks[parser.i++];
+		if (tok->type != JSMN_STRING)
+			return 0;
+
+		tce->subid = json + tok->start;
+		tce->subid_len = toksize(tok);
+
+		return 1;
 	}
 
 	return 0;

src/nostrdb.h
@@ -91,6 +91,7 @@ enum tce_type {
 	NDB_TCE_OK     = 0x2,
 	NDB_TCE_NOTICE = 0x3,
 	NDB_TCE_EOSE   = 0x4,
+	NDB_TCE_AUTH   = 0x5,
 };
 
 enum ndb_ingest_filter_action {

commit c201c31646f2c3b8274627e0f1fe624c7c92be3e

tce: fix build for previous TCE change
Fixes: ba86ffd06912 ("tce: add AUTH to-client-event")

src/nostrdb.c
@@ -1931,6 +1931,7 @@ static int ndb_ingester_process_event(secp256k1_context *ctx,
 		}
 	} else {
 		switch (tce.evtype) {
+		case NDB_TCE_AUTH:   goto cleanup;
 		case NDB_TCE_NOTICE: goto cleanup;
 		case NDB_TCE_EOSE:   goto cleanup;
 		case NDB_TCE_OK:     goto cleanup;